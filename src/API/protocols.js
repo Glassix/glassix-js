18fc89e65fb5414f19ff10b26f496152a1a5b42b
Julia Zasipko <julia.zasipko@glassix.com>
2023-07-27 16:48:22 +0300

    error handling

--- src/API/protocols.js
+++ src/API/protocols.js
@@ -2,9 +2,16 @@ import axios from 'axios';
 
 // PROTOCOLS ENDPOINTS
 const sendProtocol = async (ctx, payload = {}) => {
-  const headers = await ctx.getRequestHeaders(ctx);
-  const res = await axios.post(`${ctx.url}/protocols/send`, payload, { headers });
-  return res?.data;
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.post(`${ctx.url}/protocols/send`, payload, {headers});
+		return res?.data;
+	} catch (error) {
+		return {
+			statusCode: error?.response?.status,
+			message: error?.response?.data?.Message
+		};
+	}
 };
 
 export default sendProtocol;
--- src/API/users.js
+++ src/API/users.js
@@ -2,55 +2,118 @@ import axios from 'axios';
 
 // USERS ENDPOINTS
 export const getAllUsers = async (ctx) => {
-  const headers = await ctx.getRequestHeaders(ctx);
-  const res = await axios.get(`${ctx.url}/users/allusers`, { headers });
-  return res?.data;
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.get(`${ctx.url}/users/allusers`, {headers});
+		return res?.data;
+	} catch (error) {
+		return {
+			statusCode: error?.response?.status,
+			message: error?.response?.data?.Message
+		};
+	}
 };
 
 export const setUserStatus = async (ctx, payload = {}) => {
-  const headers = await ctx.getRequestHeaders(ctx);
-  const res = await axios.put(`${ctx.url}/users/setstatus`, payload, { headers });
-  return res?.data;
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.put(`${ctx.url}/users/setstatus`, payload, {headers});
+		return res?.data;
+	} catch (error) {
+		return {
+			statusCode: error?.response?.status,
+			message: error?.response?.data?.Message
+		};
+	}
 };
 
 export const getUserStatus = async (ctx) => {
-  const headers = await ctx.getRequestHeaders(ctx);
-  const res = await axios.get(`${ctx.url}/users/getstatus`, { headers });
-  return res?.data;
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.get(`${ctx.url}/users/getstatus`, {headers});
+		return res?.data;
+	} catch (error) {
+		return {
+			statusCode: error?.response?.status,
+			message: error?.response?.data?.Message
+		};
+	}
 };
 
 export const addUser = async (ctx, payload = {}, params = {}) => {
-  const headers = await ctx.getRequestHeaders(ctx);
-  const res = await axios.post(`${ctx.url}/users/add`, payload, { headers, params });
-  return res?.data;
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.post(`${ctx.url}/users/add`, payload, {headers, params});
+		return res?.data;
+	} catch (error) {
+		return {
+			statusCode: error?.response?.status,
+			message: error?.response?.data?.Message
+		};
+	}
 };
 
 export const deleteUser = async (ctx, params = {}) => {
-  const headers = await ctx.getRequestHeaders(ctx);
-  const res = await axios.delete(`${ctx.url}/users/delete`, { headers, params });
-  return res?.data;
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.delete(`${ctx.url}/users/delete`, {headers, params});
+		return res?.data;
+	} catch (error) {
+		return {
+			statusCode: error?.response?.status,
+			message: error?.response?.data?.Message
+		};
+	}
 };
 
 export const setUserUniqueArgument = async (ctx, payload = {}) => {
-  const headers = await ctx.getRequestHeaders(ctx);
-  const res = await axios.put(`${ctx.url}/users/setuniqueargument`, payload, { headers });
-  return res?.data;
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.put(`${ctx.url}/users/setuniqueargument`, payload, {headers});
+		return res?.data;
+	} catch (error) {
+		return {
+			statusCode: error?.response?.status,
+			message: error?.response?.data?.Message
+		};
+	}
 };
 
 export const updateUser = async (ctx, payload = {}) => {
-  const headers = await ctx.getRequestHeaders(ctx);
-  const res = await axios.post(`${ctx.url}/users/update`, payload, { headers });
-  return res?.data;
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.post(`${ctx.url}/users/update`, payload, {headers});
+		return res?.data;
+	} catch (error) {
+		return {
+			statusCode: error?.response?.status,
+			message: error?.response?.data?.Message
+		};
+	}
 };
 
 export const getUserByUniqueArgument = async (ctx, params = {}) => {
-  const headers = await ctx.getRequestHeaders(ctx);
-  const res = await axios.get(`${ctx.url}/users/getbyuniqueargument`, { headers, params });
-  return res?.data;
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.get(`${ctx.url}/users/getbyuniqueargument`, {headers, params});
+		return res?.data;
+	} catch (error) {
+		return {
+			statusCode: error?.response?.status,
+			message: error?.response?.data?.Message
+		};
+	}
 };
 
 export const setUserRoles = async (ctx, payload = {}, params = {}) => {
-  const headers = await ctx.getRequestHeaders(ctx);
-  const res = await axios.post(`${ctx.url}/users/setroles`, payload, { headers, params });
-  return res?.data;
+	try {
+		const headers = await ctx.getRequestHeaders(ctx);
+		const res = await axios.post(`${ctx.url}/users/setroles`, payload, {headers, params});
+		return res?.data;
+	} catch (error) {
+		return {
+			statusCode: error?.response?.status,
+			message: error?.response?.data?.Message
+		};
+	}
 };
