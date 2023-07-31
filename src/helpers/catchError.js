619d5e03644268baca795dccb686799a6fb60e46
Yoad Rashty <yoad.rashty@glassix.com>
2023-07-31 12:44:44 +0300

    remove debugger;

--- src/helpers/catchError.js
+++ src/helpers/catchError.js
@@ -1,7 +1,7 @@
 const catchError = (error) => {
   const err = new Error(error?.response?.data?.message || error?.response?.data?.Message || 'an error has occurred"');
   err.code = error?.response?.status;
-debugger;
+
   console.error(`Status code: ${err.code} message: ${err}`);
   throw err;
 };
