a176d82cdaa2d9522551bafbb1d5888554ad8270
Yoad Rashty <yoad.rashty@glassix.com>
2023-07-31 16:24:22 +0300

    env vars

--- examples/example.js
+++ examples/example.js
@@ -1,21 +1,22 @@
+require('dotenv').config();
 const glassix = require('glassix');
 
 // When working with npm packages you often want to test your package without having to publish it to npm.
 // This enables you to have a quicker feedback loop and keeps the number of published versions to a minimum.
 
+// Run in terminal in examples folder: npm link glassix
 // Run in terminal: npm run test:examples
 
 console.log("Runing example.js");
 
-// Your glassix subdomain
-const workspace = 'YOUR_WORKSPACE';
-// Find your key and secret on Settings → Developers → Api keys
-const apiKey = 'YOUR_API_KEY';
-const apiSecret = 'YOUR_API_SECRET';
-// a user with access to your department, preferably an API user
-const userName = 'USER_NAME';
+const test = async ()=> {
+    const client = new glassix(process.env.WORKSPACE, process.env.API_KEY, process.env.API_SECRET, process.env.USER_NAME);
 
-
-const client = new glassix(workspace, apiKey, apiSecret, userName);
-
-client.tickets.get(0);
+    // get ticket
+    let ticket = await client.tickets.get(53001859);
+    
+    // update it's tags
+    const nextTags = ['Sales'];
+    const result = await client.tickets.addTags(ticket.id, nextTags);
+};
+test();
