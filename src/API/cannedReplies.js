155c972c9b50bde0d918122241c17129e75b5036
Vered Magen <vered.magen@glassix.com>
2026-06-24 14:43:52 +0300

    feature/GSP-5676- add new canned replies endpoints

--- src/API/cannedReplies.js
+++ src/API/cannedReplies.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import catchError from '../helpers/catchError';
 
 // CANNED REPLIES ENDPOINTS
-const getAllCannedReplies = async (ctx) => {
+export const getAllCannedReplies = async (ctx) => {
   try {
     const headers = await ctx.getRequestHeaders(ctx);
     const res = await axios.get(`${ctx.url}/cannedreplies/getall`, { headers });
@@ -12,4 +12,27 @@ const getAllCannedReplies = async (ctx) => {
   }
 };
 
-export default getAllCannedReplies;
+export const addCannedReply = async (ctx, payload) => {
+  try {
+    const headers = await ctx.getRequestHeaders(ctx);
+    const res = await axios.post(`${ctx.url}/cannedreplies/add`, payload, {
+      headers,
+    });
+    return res?.data;
+  } catch (error) {
+    return catchError(error);
+  }
+};
+
+export const deleteCannedReply = async (ctx, params = {}) => {
+  try {
+    const headers = await ctx.getRequestHeaders(ctx);
+    const res = await axios.delete(`${ctx.url}/cannedreplies/delete`, {
+      headers,
+      params,
+    });
+    return res?.data;
+  } catch (error) {
+    return catchError(error);
+  }
+};
